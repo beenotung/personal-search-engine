ce59a37dfac985f0e6a13eb659811c01aaadf93b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-09-01 01:41:53 +0800

    install sqlite

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+import DB from 'better-sqlite3-helper'
+import { toSafeMode } from 'better-sqlite3-schema'
+import { join } from 'path'
+
+export let db = DB({
+  path: join('data', 'sqlite3.db'),
+  migrate: {
+    migrationsPath: 'migrations',
+    table: 'migrations',
+    force: false,
+  },
+})
+
+toSafeMode(db)
