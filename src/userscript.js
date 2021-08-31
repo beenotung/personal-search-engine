564c71dfce791e683f948ba159523f1f004576d1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-09-01 01:42:02 +0800

    add userscript skeleton

---
+++ src/userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         personal-search-engine
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  remove iframe from the page
+// @author       Beeno Tung
+// @include      http://*
+// @include      https://*
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  'use strict'
+
+  console.log('personal-search-engine v0.1')
+})()
