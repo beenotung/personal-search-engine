d14953cf5beae081ccd6b94c9eb0288c445d355e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-09-02 09:15:48 +0800

    detect page switching in SPA (single-page-webapp)

--- src/userscript.js
+++ src/userscript.js
@@ -15,13 +15,20 @@
   console.log('personal-search-engine v0.1')
 
   let skipMetaNameList = ['regionsAllowed', /:url/]
-  let skipSelectorList = [
-    'svg',
-    'script',
-    'iframe',
-    'link',
-    'style',
-    '.TridactylStatusIndicator',
+  let urlMatchSkipSelectorList = [
+    [
+      /.*/,
+      [
+        'svg',
+        'script',
+        'iframe',
+        'link',
+        'style',
+        '.TridactylStatusIndicator',
+        'noscript',
+      ],
+    ],
+    [/https:\/\/www\.youtube\.com\/watch\?/, ['.playlist-items', '#related']],
   ]
 
   function compare(a, b) {
@@ -30,69 +37,122 @@
     return 0
   }
 
-  let meta_list = []
-  document.querySelectorAll('meta[content]').forEach((meta, index) => {
-    attr: for (let i = 0; i < meta.attributes.length; i++) {
-      let attr = meta.attributes.item(i)
-      if (attr.nodeName === 'content') continue
-      let key = attr.nodeValue
-      for (let skipName of skipMetaNameList) {
-        if (key.match(skipName)) {
-          continue attr
+  function main() {
+    let meta_list = []
+    document.querySelectorAll('meta[content]').forEach((meta, index) => {
+      attr: for (let i = 0; i < meta.attributes.length; i++) {
+        let attr = meta.attributes.item(i)
+        if (attr.nodeName === 'content') continue
+        let key = attr.nodeValue
+        for (let skipName of skipMetaNameList) {
+          if (key.match(skipName)) {
+            continue attr
+          }
         }
+        let type = attr.nodeName
+        let value = meta.attributes.getNamedItem('content').nodeValue
+        meta_list.push({ index, type, key, value })
       }
-      let type = attr.nodeName
-      let value = meta.attributes.getNamedItem('content').nodeValue
-      meta_list.push({ index, type, key, value })
-    }
-  })
-  meta_list = meta_list
-    .sort((a, b) => compare(a.value, b.value) || compare(a.index, b.index))
-    .filter((x, i, xs) => i === 0 || x.value !== xs[i - 1].value)
-    .sort((a, b) => compare(a.index, b.index))
-    .map(meta => {
-      delete meta.index
-      return meta
     })
+    meta_list = meta_list
+      .sort((a, b) => compare(a.value, b.value) || compare(a.index, b.index))
+      .filter((x, i, xs) => i === 0 || x.value !== xs[i - 1].value)
+      .sort((a, b) => compare(a.index, b.index))
+      .map(meta => {
+        delete meta.index
+        return meta
+      })
 
-  let body = document.createElement('body')
-  body.innerHTML = document.body.innerHTML
+    let body = document.createElement('body')
+    body.innerHTML = document.body.innerHTML
 
-  skipSelectorList.forEach(tag =>
-    body.querySelectorAll(tag).forEach(e => e.remove()),
-  )
-  body.querySelectorAll('*').forEach(e => {
-    let styles = getComputedStyle(e)
-    if (styles.display === 'none') {
-      e.remove()
-      return
+    urlMatchSkipSelectorList.forEach(
+      ([urlMatch, selectorList]) =>
+        location.href.match(urlMatch) &&
+        selectorList.forEach(selector =>
+          body.querySelectorAll(selector).forEach(e => e.remove()),
+        ),
+    )
+    if (location.href.startsWith('')) {
+      body.querySelectorAll('.playlist-items')
+      ;('#related')
     }
-    if (e.tagName === 'IMG' && e.alt) {
-      e.outerHTML = `<span>${e.alt}</span>`
-      return
-    }
-    if (!e.textContent.trim()) {
-      e.remove()
-      return
-    }
-  })
-  let text = Array.from(
-    new Set(
-      body.textContent
-        .split('\n')
-        .map(line => line.trim())
-        .filter(line => line),
-    ),
-  ).join('\n')
+    body.querySelectorAll('*').forEach(e => {
+      let styles = getComputedStyle(e)
+      if (styles.display === 'none') {
+        e.remove()
+        return
+      }
+      if (e.tagName === 'IMG' && e.alt) {
+        e.outerHTML = `<span>${e.alt}</span>`
+        return
+      }
+      if (!e.textContent.trim()) {
+        e.remove()
+        return
+      }
+    })
+    let text = Array.from(
+      new Set(
+        body.textContent
+          .split('\n')
+          .map(line => line.trim())
+          .filter(line => line),
+      ),
+    ).join('\n')
 
-  let title = document.title
-  let url = location.href
-  let data = {
-    url,
-    title,
-    meta_list,
-    text,
+    let title = document.title
+    let url = location.href
+    let page = {
+      url,
+      title,
+      meta_list,
+      text,
+    }
+    console.log('report page:', {
+      title,
+      url,
+      text_length: text.length,
+    })
+    fetch('http://localhost:8090/page', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ page }),
+    })
   }
-  console.log('page data:', data)
-  console.log('text length:', text.length)
+
+  let lastUrl
+
+  function wait(acc, url) {
+    if (location.href !== url) {
+      return
+    }
+    // console.log('wait:', acc)
+    if (acc <= 0) {
+      main()
+      return
+    }
+    requestIdleCallback(() => wait(acc - 1, url))
+  }
+
+  function start(event) {
+    if (lastUrl === location.href) return
+    lastUrl = location.href
+    console.log('start from:', event)
+    wait(20, lastUrl)
+  }
+
+  window.addEventListener('popstate', start)
+  window.addEventListener('hashchange', start)
+  new MutationObserver(mutations => {
+    start('dom mutation')
+  }).observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    characterData: true,
+  })
+  start('init')
 })()
