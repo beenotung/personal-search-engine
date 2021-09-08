3e6a8c9aa1192dfba3ce18db412371cfdb409bd8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-09-09 07:56:38 +0800

    change console log in server.ts to use 'debug' package

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import { Userscript } from './models'
 import { storePage, deletePages } from './service'
 import { filterPage } from './controller'
 import { renderSearchPage } from './view'
+import debug from 'debug'
+
+const log = debug('search-engine:server.ts')
+log.enabled = true
 
 const app = express()
 
@@ -24,18 +28,18 @@ app.post('/page', (req, res) => {
   const page = req.body.page as Userscript.Page
   const result = filterPage(page)
   if (result === 'skip') {
-    console.log('skip page:', page.url)
+    log('skip page:', page.url)
     res.json('skipped')
     return
   }
-  console.log('store page:', page.url)
+  log('store page:', page.url)
   storePage(page)
   res.json('stored')
 })
 
 app.post('/delete', (req, res) => {
   const page_id_list = Object.keys(req.body)
-  console.log('delete pages:', page_id_list)
+  log('delete pages:', page_id_list)
   deletePages(page_id_list)
   res.redirect('/')
 })
