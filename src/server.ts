03a24ed612dbc9b200e98c74e6cf4327b3fa5748
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-09-09 07:51:26 +0800

    fix deprecation warning

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.end(renderSearchPage())
